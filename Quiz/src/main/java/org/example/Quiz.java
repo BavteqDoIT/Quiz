6ecf8f3e0829845758a01a5480046bd05cebced5
BavteqDoIT <125410325+BavteqDoIT@users.noreply.github.com>
2024-03-19 21:13:37 +0100

    Add files via upload

---
+++ Quiz/src/main/java/org/example/Quiz.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Quiz {
+    File quizJson;
+    public Quiz(String fileName){
+        String file = Objects.requireNonNull(this.getClass().getClassLoader().getResource(fileName)).getFile();
+        quizJson = new File(file);
+    }
+
+    public void displayQuestions() throws IOException {
+        ObjectMapper mapper= new ObjectMapper();
+        List<Question> questions = mapper.readValue(quizJson, new TypeReference<>() {});
+
+        questions.stream()
+                .map(Question::getPytanie)
+                .forEach(System.out::println);
+    }
+
+    public void play() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int points = 0;
+        ObjectMapper mapper= new ObjectMapper();
+        List<Question> questions = mapper.readValue(quizJson, new TypeReference<>() {});
+
+        for (Question question : questions){
+            System.out.println(question.getPytanie());
+            System.out.println("a: " + question.getA());
+            System.out.println("b: " + question.getB());
+            System.out.println("c: " + question.getC());
+            System.out.println("d: " + question.getD());
+
+            String answer = scanner.nextLine();
+
+            if(question.getPrawidlowaOdpowiedz().equals(answer)) {
+                System.out.println("To prawidlowa odpowiedź!" + "\n");
+                points++;
+            } else {
+                System.out.println("Niestety to błędna odpowiedź. Prawidłowa odpowiedź to "
+                    + question.getPrawidlowaOdpowiedz() + "\n");
+            }
+        }
+
+        System.out.println("To już koniec gry. Twoja liczba punktór to " + points);
+        scanner.close();
+    }
+}
